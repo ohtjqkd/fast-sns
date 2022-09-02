0ebb3cb09e1c11eb987e781e87e1212bdc2e5ffe
ohtjqkd <ohtjqkd@gmail.com>
2022-09-03 01:30:17 +0900

    feature: modify main

--- src/main/java/com/fastcampus/fastsns/FastSnsApplication.java
+++ src/main/java/com/fastcampus/fastsns/FastSnsApplication.java
@@ -2,8 +2,9 @@ package com.fastcampus.fastsns;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class FastSnsApplication {
 
     public static void main(String[] args) {
