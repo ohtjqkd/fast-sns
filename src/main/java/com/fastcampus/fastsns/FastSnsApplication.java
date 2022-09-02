b582dc32333e2f6fdd15b772301441b6b8f0f158
ohtjqkd <ohtjqkd@gmail.com>
2022-09-03 00:10:57 +0900

    Initial commit

---
+++ src/main/java/com/fastcampus/fastsns/FastSnsApplication.java
@@ -0,0 +1,13 @@
+package com.fastcampus.fastsns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastSnsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FastSnsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fastcampus/fastsns/FastSnsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastcampus.fastsns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastSnsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
